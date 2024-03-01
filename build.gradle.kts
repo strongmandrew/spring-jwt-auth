da30e7b2e572f8ad48c5038962cf68ca843703c9
Andrew <vad30001@gmail.com>
2024-03-01 23:14:45 +0300

    Initial setup of Thymeleaf

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.strongmandrew"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    /*implementation("org.springframework.boot:spring-boot-starter-security")*/
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    /*implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")*/
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    /*testImplementation("org.springframework.security:spring-security-test")*/
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/strongmandrew/springjwtauth/SpringJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.strongmandrew.springjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtAuthApplication.class, args);
+    }
+
+}
